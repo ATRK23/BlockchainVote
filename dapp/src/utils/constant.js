5e1d3acbd1013cf1d0dd39f098e235fd41b3a8c4
samyamine <halitsamyamine01@yahoo.com>
2025-06-17 01:09:34 +0200

    Ajout de constant.js avec l'ABI, l'adresse du contrat et le network ID

---
+++ dapp/src/utils/constant.js
@@ -0,0 +1,11 @@
+import voteContract from "./contracts/Vote.json";
+
+export const CONTRACT_ABI = voteContract.abi;
+
+// ⚠️ À adapter après chaque déploiement local
+// Adresse du contrat déployé sur Ganache 
+export const CONTRACT_ADDRESS = "0xb92D50aFC71c4e6f84c41De25F89a07CA89A1891";
+
+// ⚠️ À adapter après chaque déploiement local
+// ID du réseau Ganache 
+export const NETWORK_ID = "1750111615810"; 
