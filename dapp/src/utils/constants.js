8bc30c85d66f6f979732f1b9ca9d38eddc683d69
Arthur BAILLET <arthur.baillet@gmail.com>
2025-06-19 23:31:21 +0200

    [BUG] Changement du network_id par défaut et des chemins pour ABI et ADDRESS

--- dapp/src/utils/constants.js
+++ dapp/src/utils/constants.js
@@ -1,11 +1,5 @@
-import voteContract from "../contracts/Vote.json";
+import contractJson from "../contracts/Vote.json";
 
-export const CONTRACT_ABI = voteContract.abi;
-
-// ⚠️ À adapter après chaque déploiement local
-// Adresse du contrat déployé sur Ganache 
-export const CONTRACT_ADDRESS = "0xb92D50aFC71c4e6f84c41De25F89a07CA89A1891";
-
-// ⚠️ À adapter après chaque déploiement local
-// ID du réseau Ganache 
-export const NETWORK_ID = "1337"; 
+export const CONTRACT_ABI = contractJson.abi;
+export const CONTRACT_ADDRESS = contractJson.address;
+export const NETWORK_ID = "1337";
